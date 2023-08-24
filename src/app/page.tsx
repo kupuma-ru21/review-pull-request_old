d60c2cdde0f4065fd6d5ab9a4bc930c6e93b0412
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-24 22:33:48 +0900

    chore: add rule on eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { css } from '@/styled-system/css';
 
-export default function Home() {
-  return (
-    <h1 className={css({ fontSize: '16px', fontWeight: 'bold' })}>Home</h1>
-  );
-}
+const Home = () => (
+  <h1 className={css({ fontSize: '16px', fontWeight: 'bold' })}>Home</h1>
+);
+
+export default Home;
