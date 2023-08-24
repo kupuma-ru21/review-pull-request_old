9323e948f96fba95e2640de9afbc8dde782d415f
木村 浩一 <koi.kimura@buysell-technologies.com>
2023-08-24 11:57:43 +0900

    chore: add stylelint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { css } from '../../styled-system/css';
 
 export default function Home() {
-  return <h1 className={css({ fontSize: '8xl', fontWeight: 'bold' })}>Home</h1>;
+  return (
+    <h1 className={css({ fontSize: '16px', fontWeight: 'bold' })}>Home</h1>
+  );
 }
