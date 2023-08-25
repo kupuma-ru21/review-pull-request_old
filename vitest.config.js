835c69eff3d72519b9f79bd3b908ff0f81ac41d9
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-25 22:01:52 +0900

    chore: change tsconfig

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // FIXME: https://github.com/vitest-dev/vitest/discussions/1573#discussioncomment-3653362
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     environment: 'jsdom',
     setupFiles: ['./tests/setup.ts'],
