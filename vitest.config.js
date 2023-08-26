5a35980232d1ead016503bbb5f2fa6504f7b608b
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-26 14:37:13 +0900

    revert: to pass ci

--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // FIXME: https://github.com/vitest-dev/vitest/discussions/1573#discussioncomment-3653362
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   test: {
     environment: 'jsdom',
     setupFiles: ['./tests/setup.ts'],
